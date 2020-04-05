0078e74dd82527d603e1d4f44cfbc77cca02a66e
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2020-04-05 19:11:12 +0100

    Login and Register pages

---
+++ register.php
@@ -0,0 +1,71 @@
+<!Doctype html>
+<html>
+<head>
+	<title> Register on TuneBoxx</title>
+</head>
+<body>
+	<div id="inputContainer">
+		<form id="loginForm" action="register.php" method="POST">
+			<h1>Login to your account</h1>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g £m£ka" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginpassword" name="loginPassword" type="password" required>
+			</p>
+			
+			<button type="submit" name="loginButton"><b>LOG IN</b></button>
+		</form>
+		
+		<form id="loginForm" action="register.php" method="POST">
+			<h1>Create your free TuneBoxx Account</h1>
+			<p>
+				<label for="Username">Username</label>
+				<input id="Username" name="Username" type="text" placeholder="e.g £m£ka" required>
+			</p>
+			<p>
+				<label for="firstName">First Name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="Emeka" required>
+			</p>
+			<p>
+				<label for="lastName">Last Name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="Mike" required>
+			</p>
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="text" placeholder="e.g emeka@gmail.com" required>
+			</p>
+			<p>
+				<label for="email">Confirm Email</label>
+				<input id="email" name="email" type="text" placeholder="e.g emeka@gmail.com" required>
+			</p>
+			<p>
+				<label for="Password">Password</label>
+				<input id="password" name="Password" type="password" required>
+			</p>
+			<p>
+				<label for="Password">Confirm Password</label>
+				<input id="password" name="Password" type="password" required>
+			</p>
+			
+			<button type="submit" name="loginButton"><b>LOG IN</b></button>
+		</form>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
